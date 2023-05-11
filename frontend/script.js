aa79647e3a73cc46af561ab74fa2221414270856
praseodeveloper <praseodeveloper@gmail.com>
2023-05-11 14:08:41 +0200

    Invoke external service for results

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,59 @@
 const fqdn = `${window.location.protocol}//${window.location.host}`;
-const form = document.getElementById("form");
-form.addEventListener("submit", submitForm);
 
-function submitForm(e) {
+const validateAmlForm = document.getElementById("validateAmlForm");
+validateAmlForm.addEventListener("submit", submitValidateAmlForm);
+
+const getMLFBForm = document.getElementById("getMLFBForm");
+getMLFBForm.addEventListener("submit", submitGetMLFBForm);
+
+const getValuesForm = document.getElementById("getValuesForm");
+getValuesForm.addEventListener("submit", submitGetValuesForm);
+
+function submitValidateAmlForm(e) {
+    this.classList.add("disabledForm");
+    document.getElementById('validateAmlResult').innerText = "";
     e.preventDefault();
-    const files = document.getElementById("files");
+    const files = document.getElementById("file");
     const formData = new FormData();
     formData.append("file", files.files[0]);
-    fetch(`${fqdn}/upload`, {
+    fetch(`${fqdn}/api/AmlValidation`, {
         method: 'POST',
         body: formData
     }).then((response) => response.json())
         .then((data) => {
-            if (data && data.wordCount) {
-                document.getElementById('result').innerText = `The word count is ${data.wordCount}`;
-            } else {
-                alert("Invalid response from server");
-            }
+            const output = data.result.split("\r\n");
+            var newline = String.fromCharCode(13, 10);
+            document.getElementById('validateAmlResult').innerText = output.join(newline);
         })
-        .catch((err) => ("Error occured", err));
+        .catch((err) => ("Error occured", err))
+        .finally(() => this.classList.remove("disabledForm"));
+}
+
+function submitGetMLFBForm(e) {
+    this.classList.add("disabledForm");
+    document.getElementById('getMLFBResult').innerText = "";
+    e.preventDefault();
+    fetch(`${fqdn}/api/Values/5`, {
+        method: 'GET',
+    }).then((response) => response.json())
+        .then((data) => {
+            document.getElementById('getMLFBResult').innerText = data.result;
+        })
+        .catch((err) => ("Error occured", err))
+        .finally(() => this.classList.remove("disabledForm"));
+}
+
+
+function submitGetValuesForm(e) {
+    this.classList.add("disabledForm");
+    document.getElementById('getValuesResult').innerText = "";
+    e.preventDefault();
+    fetch(`${fqdn}/api/Values`, {
+        method: 'GET',
+    }).then((response) => response.json())
+        .then((data) => {
+            document.getElementById('getValuesResult').innerText = data.result;
+        })
+        .catch((err) => ("Error occured", err))
+        .finally(() => this.classList.remove("disabledForm"));
 }
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const fs = require('fs');
-var http = require('http');
+const http = require('http');
+const pd = require('pretty-data').pd;
 
 const port = 8989;
+
+const opennessHost = '18.159.149.25';
+const opennessPort = 10281;
 
 ["/", "/index.html"].forEach(function (entryPoint) {
     app.get(entryPoint, (req, res) => {
@@ -37,57 +41,99 @@ app.post('/upload', upload.single('file'), (req, res) => {
     }
 });
 
-// app.post('/validateAml', upload.single('file'), (req, res) => {
-//     if (req.file) {
-//         const filePath = req.file.path;
-//         fs.readFile(req.file.path, 'utf8', (err, contents) => {
-//             if (err) {
-//                 res.status(500).send({ error: err });
-//                 return;
-//             }
+app.post('/api/AmlValidation', upload.single('file'), (req, res) => {
+    if (req.file) {
+        const filePath = req.file.path;
+        fs.readFile(req.file.path, 'utf8', (err, contents) => {
+            if (err) {
+                res.status(500).send({ error: err });
+                return;
+            }
 
-//             const urlparams = {
-//                 host: '18.159.149.25', //No need to include 'http://' or 'www.'
-//                 port: 10281,
-//                 timeout: 180000,
-//                 path: '/api/AmlValidation',
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json; charset=UTF-8', //Specifying to the server that we are sending JSON 
-//                 }
-//             };
+            const urlparams = {
+                host: opennessHost, //No need to include 'http://' or 'www.'
+                port: opennessPort,
+                timeout: 180000,
+                path: '/api/AmlValidation',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8', //Specifying to the server that we are sending JSON 
+                }
+            };
 
-//             function onResponse(response) {
-//                 let data = '';
+            function onResponse(response) {
+                let data = '';
 
-//                 response.on('data', function (chunk) {
-//                     data += chunk; //Append each chunk of data received to this variable.
-//                 });
-//                 response.on('end', function () {
-//                     res.json({ "result": data });
-//                     console.log(data); //Display the server's response, if any.
-//                 });
-//                 response.on('error', function (err) {
-//                     res.status(500).send({ "error": err });
-//                     console.error(err);
-//                 });
-//             }
+                response.on('data', function (chunk) {
+                    data += chunk; //Append each chunk of data received to this variable.
+                });
+                response.on('end', function () {
+                    res.json({ "result": data });
+                    console.log(data); //Display the server's response, if any.
+                });
+                response.on('error', function (err) {
+                    res.status(500).send({ "error": err });
+                    console.error(err);
+                });
+            }
 
-//             try {
-//                 var request = http.request(urlparams, onResponse); //Create a request object.
-//                 request.write(JSON.stringify({
-//                     "AmlContent": contents
-//                 })); //Send off the request.
-//                 request.end(); //End the request.
-//             }
-//             catch (error) {
-//                 console.error(error);
-//             }
-//         });
-//     } else {
-//         res.status(500).send({ error: "No file uploaded to server" });
-//     }
-// });
+            try {
+                var request = http.request(urlparams, onResponse); //Create a request object.`
+                // contents = contents.replace(/\r\n/g, '');
+                contents = pd.xmlmin(contents, false);
+                var requestBody = JSON.stringify({ "AmlContent": contents });
+                console.log(`Request body : ${requestBody}`);
+                request.write(requestBody); //Send off the request.
+                request.end(); //End the request.
+            }
+            catch (error) {
+                console.error(error);
+            }
+        });
+    } else {
+        res.status(500).send({ error: "No file uploaded to server" });
+    }
+});
+
+["/api/Values", "/api/Values/5"].forEach(function (endpoint) {
+    app.get(endpoint, (req, res) => {
+
+        const urlparams = {
+            host: opennessHost, 
+            port: opennessPort,
+            timeout: 180000,
+            path: endpoint,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        };
+
+        function onResponse(response) {
+            let data = '';
+
+            response.on('data', function (chunk) {
+                data += chunk; //Append each chunk of data received to this variable.
+            });
+            response.on('end', function () {
+                res.json({ "result": data });
+                console.log(data); //Display the server's response, if any.
+            });
+            response.on('error', function (err) {
+                res.status(500).send({ "error": err });
+                console.error(err);
+            });
+        }
+
+        try {
+            var request = http.request(urlparams, onResponse); //Create a request object.
+            request.end(); //End the request.
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
